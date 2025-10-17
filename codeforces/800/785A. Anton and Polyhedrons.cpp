b671b00bd8c7742702347e8932c73eed9ee8f09a
HiT1000 <hiteshsingla209@gmail.com>
2025-10-17 16:01:46 +0530

    800

---
+++ codeforces/800/785A. Anton and Polyhedrons.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+#define ll long long
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vecin(name, len) vector<int> name(len); for (auto &_ : name) cin >> _;
+#define vecout(v) for (auto _ : v) cout << _ << ' '; cout << endl;
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repr(i, a, b) for(int i = a; i >= b; i--)
+
+#define debug(x) cerr << #x << " = " << x << '\n'
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+
+int main() {
+    fastio;
+    
+    int t,count=0;
+    cin >> t;
+    string s;
+
+    for(int i=0; i<t; i++){
+        // getline(cin,s);
+        cin>>s;
+        // cout<<s<<endl;
+        if(s == "Tetrahedron"){
+            count+=4;
+        }
+        else if(s == "Cube"){
+            count+=6;
+        }
+        else if(s == "Octahedron"){
+            count+=8;
+        }
+        else if(s == "Dodecahedron"){
+            count+=12;
+        }
+        else if(s == "Icosahedron"){
+            count+=20;
+        }
+    }
+    cout<<count;
+    
+    return 0;
+}
